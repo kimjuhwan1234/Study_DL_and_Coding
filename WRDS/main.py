41088a2b1c5483f12b5d80355bbf9a1a8a934136
kimjuhwan1234 <juhwankim@hanyang.ac.kr>
2025-10-01 11:24:36 +0900

    2025-10-01

--- WRDS/main.py
+++ WRDS/main.py
@@ -1,29 +1,33 @@
-import pandas as pd
 import os
+import pandas as pd
 import common as c
 
 
 def checkdir():
-    for folder in ['input', 'output_direct', 'output_dropna',
-                   'output_dropna/simple', 'output_dropna/smart', 'output_dropna/smartexmom']:
+    for folder in ['WRDS/Database', 'WRDS/output_direct', 'WRDS/output_dropna',
+                   'WRDS/output_dropna/simple', 'WRDS/output_dropna/smart', 'WRDS/output_dropna/smartexmom']:
         if not os.path.exists(folder):
             os.makedirs(folder)
 
 
 def green2output():
     # Convert Green's file(var `temp2` in the sas code) into our format (files separated by date)
-    chars = pd.read_csv(os.path.join('input', '_characteristics.csv'))[
+    chars = pd.read_csv(os.path.join('WRDS/Database', '_characteristics.csv'), encoding='iso-8859-1')[
         'name'].tolist()
-    necessary_cols = [item.lower() for item in chars.copy() + ['gvkey', 'DATE', 'prc']]
+    necessary_cols = [item.lower()
+                      for item in chars.copy() + ['gvkey', 'DATE', 'prc']]
 
-    df = pd.read_csv(os.path.join('input', 'green.csv'), low_memory=False)
+    df = pd.read_parquet(os.path.join('WRDS/Database', 'green.parquet'))
     df = df[df.columns[df.columns.str.lower().isin(necessary_cols)]]
-    df = df.dropna(subset=['gvkey', 'DATE']).drop(index=163354)  # somehow that row is duplicated
-    df['DATE'] = pd.to_datetime(df['DATE'], format='%Y%m%d').dt.strftime('%Y-%m-%d')
+    df = df.dropna(subset=['gvkey', 'DATE']).drop(
+        index=163354)  # somehow that row is duplicated
+    df['DATE'] = pd.to_datetime(
+        df['DATE'], format='%Y%m%d').dt.strftime('%Y-%m-%d')
 
     # pivot the price dataframe
     prc_df = df[['gvkey', 'DATE', 'prc']]
-    prc_df = prc_df.pivot(index='DATE', columns='gvkey', values='prc').sort_index()
+    prc_df = prc_df.pivot(index='DATE', columns='gvkey',
+                          values='prc').sort_index()
     mom_df = prc_df / prc_df.shift(1) - 1
     """
     At this stage, mom_df looks like:
@@ -48,12 +52,13 @@ def green2output():
     Note that 62.400002/69.300003 - 1 = -0.099567, 59.360001/62.400002 - 1 = -0.002856, etc.
     That is, `YYYY-MM-DD` in mom_df contains the return from `YYYY-(MM-1)-DD` to `YYYY-MM-DD`.
     """
-    mom_df.to_csv(os.path.join('output_direct', '_momentum.csv'), index=True)
+    mom_df.to_csv(os.path.join('WRDS/output_direct',
+                  '_momentum.csv'), index=True)
     del mom_df
 
     # datadate column contains last days of the month
     all_dates = pd.Series(df['DATE'].unique()).sort_values()
-    aa=df['DATE'].unique()[0][:4]
+    aa = df['DATE'].unique()[0][:4]
     print(f'Converting Green\'s data\n{aa} [', end='')
     for date in all_dates.iloc[1:]:
         charm_df = df[df['DATE'] == date]
@@ -63,12 +68,15 @@ def green2output():
         # add mom/price data
         todays_index = prc_df.index.get_loc(date)
         last_month = prc_df.index[todays_index - 1]  # YYYY-(MM-1)-DD
-        t48m_prc_df = prc_df.iloc[max(todays_index - 48, 0):todays_index]  # YYYY-(MM-48)-DD to YYYY-(MM-1)-DD
+        # YYYY-(MM-48)-DD to YYYY-(MM-1)-DD
+        t48m_prc_df = prc_df.iloc[max(todays_index - 48, 0):todays_index]
         momm_df = t48m_prc_df.iloc[-1]/t48m_prc_df - 1  # mom_n where n > 1
-        momm_df.loc[last_month] = prc_df.iloc[todays_index]/prc_df.loc[last_month] - 1  # mom_1
+        momm_df.loc[last_month] = prc_df.iloc[todays_index] / \
+            prc_df.loc[last_month] - 1  # mom_1
 
         # rename columns
-        ind_dict = pd.Series(momm_df.index).sort_values(ascending=False).reset_index(drop=True).to_dict()
+        ind_dict = pd.Series(momm_df.index).sort_values(
+            ascending=False).reset_index(drop=True).to_dict()
         ind_dict = {ind_dict[i]: 'mom_' + str(i+1) for i in ind_dict}
         momm_df = momm_df.rename(index=ind_dict)
         momm_df = momm_df.dropna(axis=1, how='all').T
@@ -94,34 +102,42 @@ def green2output():
         """
 
         charm_df = charm_df.join(momm_df, how='left')
-        charm_df.to_csv(os.path.join('output_direct', date + '.csv'), index=True)
+        charm_df.to_csv(os.path.join(
+            'WRDS/output_direct', date + '.csv'), index=True)
 
         print('-', end='')
         print(f']\n{int(date[:4])+1} [', end='') if '-12-' in date else None
 
-    print('\n\nOverall NaN ratio:', round(df.isna().sum().sum() / df.size * 100, 2), '%\n\n')
+    print('\n\nOverall NaN ratio:', round(
+        df.isna().sum().sum() / df.size * 100, 2), '%\n\n')
     del df, prc_df
 
 
 def direct2dropnas():
-    start_year = c.listdir('output_direct')[0][:4]
+    start_year = c.listdir('WRDS/output_direct')[0][:4]
     print(f'Dropping NaNs\n{start_year} [', end='')
-    for afile in c.listdir('output_direct', '.csv'):
+    for afile in c.listdir('WRDS/output_direct', '.csv'):
         if afile.startswith('_'):
             continue
-        df = pd.read_csv(os.path.join('output_direct', afile)).set_index('gvkey')
+        df = pd.read_csv(os.path.join(
+            'WRDS/output_direct', afile)).set_index('gvkey')
         df_simple = df.dropna(axis=1, how='any')
-        df_simple.to_csv(os.path.join('output_dropna', 'simple', afile), index=True)
+        df_simple.to_csv(os.path.join(
+            'WRDS/output_dropna', 'simple', afile), index=True)
         df_smart = c.smartdropna(df)
-        df_smart.to_csv(os.path.join('output_dropna', 'smart', afile), index=True)
-        df_smart2 = c.smartdropna(df, exclude=['mom_'+str(i) for i in range(1, 49)])
-        df_smart2.to_csv(os.path.join('output_dropna', 'smartexmom', afile), index=True)
+        df_smart.to_csv(os.path.join(
+            'WRDS/output_dropna', 'smart', afile), index=True)
+        df_smart2 = c.smartdropna(
+            df, exclude=['mom_'+str(i) for i in range(1, 49)])
+        df_smart2.to_csv(os.path.join('WRDS/output_dropna',
+                         'smartexmom', afile), index=True)
 
         print('-', end='')
-        print(f']\n{int(afile[:4]) + 1} [', end='') if '-12-' in afile else None
+        print(f']\n{int(afile[:4]) + 1} [',
+              end='') if '-12-' in afile else None
 
 
 if __name__ == '__main__':
     checkdir()
-    # green2output()
+    green2output()
     direct2dropnas()
